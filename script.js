010878805a4e7b9a43c5eaa454191773dcdb647c
BartKK <Bartkukulka@gmail.com>
2017-03-19 14:55:35 +0100

    obiekty - działania modyfikacje

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-// Modyfikacja this apply()
-
-
-
-function logNameAll(arg) {
-
-  console.log(arg + ": " + this.name);
-}
+// obiekty - działania modyfikacje
 
 var person1 = {
-  name: 'Piter'
+  name: 'Paweł'
 };
 
-var person2 = {
-  name: 'Michel'
-};
+var person2 = new Object();
 
-var name = 'Michelle';
+// dodanie [[Put]]
+person2.name = 'Beata';
+person1.age = 22;
+person2.age = 33;
 
-// funkcja powiązana z obiektem person1
-var logNameAll1 = logNameAll.bind(person1);
-console.log(logNameAll1('person1'));
-
-var logNameAll2 = logNameAll.bind(person2, 'person2');
-console.log(logNameAll2());
-
-person2.logName = logNameAll1;
-console.log(person2.logName('person2'));
+// modyfikacji
+person1.name = 'Grześ';
+person2.name = 'Rafał';
