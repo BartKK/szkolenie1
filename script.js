c5576ef0bae9c5affe4e931dee754055c0a81303
BartKK <Bartkukulka@gmail.com>
2017-03-19 09:56:29 +0100

    poprawki

---
+++ script.js
@@ -0,0 +1,22 @@
+// literały obiektów i tablic
+
+var car = {
+
+  manufacturer: "Ford",
+  model: "Mondeo"
+};
+
+var car1 = {
+  'Manufacturer origin': "Ford",
+  model: 'Mondeo'
+};
+
+var car2;
+
+car2.manufacturer = 'Fiat';
+car2.model = '500';
+
+// literał tablic
+
+var arr = ['red', 'green', 'blue'];
+console.log(arr.[1]);
