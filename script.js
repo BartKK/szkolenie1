1a3db539dc97244bc565aa047c5428dfb66b69e9
BartKK <Bartkukulka@gmail.com>
2017-03-19 16:23:58 +0100

    NextDay

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-function keys() {
-var arrKeys = []
-var key;
-for(key in obj) {
-  if(obj.hasOwnProperty(key)){
-    arrKeys.push(key)
+
+  function nextDayDate() {
+
+        var nextDay = new Date();
+        nextDay.setDate(nextDay.getDate() + 1);
+
+        var month = nextDay.getMonth() + 1;
+        var day = nextDay.getDate();
+        var year = nextDay.getFullYear();
+        var hours = nextDay.getHours();
+        var minutes = nextDay.getMinutes();
+        var seconds = nextDay.getSeconds();
+        if (month < 10) { month = "0" + month }
+        if (day < 10) { day = "0" + day }
+
+        var jutro ="Date " + day + " - " + month + " - " + year + " Time " + hours + ":" + minutes + ":" + seconds;
+
+        document.write(jutro);
   }
-}
-return arrKeys;
-}
 
-var obj = {
-red: 'red',
-  green: 'green',
-  blue: 'blue'
-}
+nextDayDate()
 
-console.log(keys(obj)); //['red', 'green', 'blue']
+
+
+Date.prototype.addDay = function () {
+  return new Date(this.setDate(this.getDate() + 1));
+};
+
+var now = new Date();
+
+now.addDay();
