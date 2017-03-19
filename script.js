1d1450b7acc2a0871083e871e733691c601ee147
BartKK <Bartkukulka@gmail.com>
2017-03-19 13:31:32 +0100

    Przeciążenia

--- script.js
+++ script.js
@@ -1,39 +1,12 @@
-// Parametry funkcji
+// Przeciążenia (overloading)
+// C++
 
-function show(value) {
-
-  return value;
+function message(mes) {
+  console.log(mes);
 }
 
-// Array.isArray(argument) - False
-
-console.log(show('Hi')); // Hi
-console.log(show('Hi', 34)); // Hi
-console.log(show.length); // 1
-
-show = function () {
-  return arguments[0];
-
-};
-
-console.log(show('Hello')); // hello
-console.log(show('Hello', 45)); // hello
-console.log(show.length); // 0
-
-
-function sum() {
-  var result = 0;
-  var len = arguments.length;
-var i = 0;
-  while(i < len) {
-
-  result += arguments[i];
-  i++;
-  }
-  return result;
+function message() {
+  console.log('Default')
 }
 
-console.log(sum(1,5)); //6
-console.log(sum(4, 5, 7)); //16
-console.log(sum(3)); //3
-console.log(sum()); //0
+message('Hello');
