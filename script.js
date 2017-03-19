8ad3049bc3ab6d116187ad815d01e4a22a5de790
BartKK <Bartkukulka@gmail.com>
2017-03-19 14:31:24 +0100

    funkcja powiązana z obiektem person1

--- script.js
+++ script.js
@@ -17,6 +17,12 @@ var person2 = {
 
 var name = 'Michelle';
 
-logNameAll.apply(this, ['global']); // global Michelle
-logNameAll.apply(person1, ['person1']); // Piter
-logNameAll.apply(person2, ['person2']); // Michel
+// funkcja powiązana z obiektem person1
+var logNameAll1 = logNameAll.bind(person1);
+console.log(logNameAll1('person1'));
+
+var logNameAll2 = logNameAll.bind(person2, 'person2');
+console.log(logNameAll2());
+
+person2.logName = logNameAll1;
+console.log(person2.logName('person2'));
