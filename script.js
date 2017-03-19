d3698d640f15ae907d0e843053781f4e02e06d56
BartKK <Bartkukulka@gmail.com>
2017-03-19 15:19:41 +0100

    obiekty - usuwanie wartości

--- script.js
+++ script.js
@@ -1,43 +1,12 @@
-// obiekty - wykrywanie właściwości
+// Obiekty - usuwanie wartości
 
-/* NIE TAK
 var person = {
-  number: 0
+  name: 'asas'
 };
 
-if (person.number) {
-  console.log('true');
+console.log(person.name);
 
-} else {
-  console.log('false');
-}
-*/
+delete person.name;
 
-/* TO JEST OKEJ
-var person1 = {
-  name: 'asdasd',
-  age: 34
-};
-
-var person2 = {
-  name: 'Jduasdas',
-  age: 654
-};
-
-console.log('name' in person1); //true
-console.log('age' in person2); //true
-console.log('height' in person2); // false
-
-*/
-
-var person1 = {
-  name: 'asdasd',
-  logName: function () {
-    console.log(this.name);
-  }
-};
-
-console.log('name' in person1);
-console.log(person1.hasOwnProperty('name'));
-console.log('toString' in person1);
-console.log(person1.hasOwnProperty('toString'));
+console.log('name' in person);
+console.log(person.name);
