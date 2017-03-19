b206c3b85e98b1ad001a12b45317486bb24aae76
BartKK <Bartkukulka@gmail.com>
2017-03-19 15:14:18 +0100

    obiekty - wykrywanie właściwości

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-// obiekty - działania modyfikacje
+// obiekty - wykrywanie właściwości
 
-var person1 = {
-  name: 'Paweł'
+/* NIE TAK
+var person = {
+  number: 0
 };
 
-var person2 = new Object();
+if (person.number) {
+  console.log('true');
 
-// dodanie [[Put]]
-person2.name = 'Beata';
-person1.age = 22;
-person2.age = 33;
+} else {
+  console.log('false');
+}
+*/
 
-// modyfikacji
-person1.name = 'Grześ';
-person2.name = 'Rafał';
+/* TO JEST OKEJ
+var person1 = {
+  name: 'asdasd',
+  age: 34
+};
+
+var person2 = {
+  name: 'Jduasdas',
+  age: 654
+};
+
+console.log('name' in person1); //true
+console.log('age' in person2); //true
+console.log('height' in person2); // false
+
+*/
+
+var person1 = {
+  name: 'asdasd',
+  logName: function () {
+    console.log(this.name);
+  }
+};
+
+console.log('name' in person1);
+console.log(person1.hasOwnProperty('name'));
+console.log('toString' in person1);
+console.log(person1.hasOwnProperty('toString'));
