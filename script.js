7600fd7bdd92002d141fa53b0d9f79e3cd3940f6
BartKK <Bartkukulka@gmail.com>
2017-03-19 11:15:26 +0100

    Dodano typy opakowujące

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ var car1 = {
 };
 
 var car2;
-/*
+
 car2.manufacturer = 'Fiat';
 car2.model = '500';
-*/
+
 // literał tablic
 
 var arr = ['red', 'green', 'blue'];
@@ -47,3 +47,61 @@ function add() {
 }
 
 console.log(typeof add);
+
+
+var item = [];
+var obj = {};
+
+console.log(item instanceof Array); //True
+console.log(item instanceof Object); // True
+console.log(obj instanceof Object); // True
+console.log(obj instanceof Array); // False
+console.log(add instanceof Function); // True
+console.log(add instanceof Object); // True
+
+console.log(Array.isArray(item));
+
+//Typy opakowujące (primitive wrapper types)
+
+// String, Number, boolean
+
+var name = 'Bartek';
+var firstCharacter = name.charAt(0);
+console.log(firstCharacter);
+
+// to co się dzieje w tle
+
+var name1 = 'Marcin';
+var temporary = new String(name1);
+var firstCharacter1 = temporary.charAt(0);
+temporary = null;
+console.log(firstCharacter1);
+
+
+//
+
+var name = 'Grzesiek';
+name.last = 'Kowalski';
+console.log(name.last); // undefined
+
+//
+
+var name2 = 'Zosia';
+var temp = new String(name2);
+temp.last = 'Kowalska';
+temp = null;
+var temp = new String(name2);
+console.log(temp.last); //undefined
+temp = null;
+
+//
+
+var name3 = 'Genowefa';
+var count = 10;
+var found = false;
+
+console.log(name3 instanceof String); // false
+console.log(count instanceof Number); // false
+console.log(found instanceof Boolean); // false
+
+//
